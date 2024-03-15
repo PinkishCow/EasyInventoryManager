cbcbafb771b5b9f8422a9a238b038f3aeea9b37e
Christopher Flinn <17773095+PinkishCow@users.noreply.github.com>
2024-03-15 10:32:09 +0000

    Fix names

--- SamplePlugin/Classes/Inventories/Crystals.cs
+++ SamplePlugin/Classes/Inventories/Crystals.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EasyInventoryManager.Classes.Inventories
 {
-    internal class Crystal
+    internal class Crystals
     {
     }
 }
--- SamplePlugin/Classes/Inventories/IInventory.cs
+++ SamplePlugin/Classes/Inventories/IInventory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EasyInventoryManager.Classes.Inventories
 {
-    internal class IInventory
+    internal interface IInventory
     {
     }
 }
